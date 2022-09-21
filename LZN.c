3cd14a777434ddc1e80a46c903b65eeb192abe43
Лю Чжинань <827435509qq.com>
2022-09-21 16:36:55 +0300

    Move the ask name code into its own function and file.

--- LZN.c
+++ LZN.c
@@ -1,15 +1,29 @@
-#include<stdio.h>
-#include<string.h>
+/* main.c*/
+#include <stdio.h>
+
+#include "askname.h"
 
 int main(int argc, char **argv)
 {
-    char name[255];
-
-    printf("Enter your name: ");
-    fgets(name, 255, stdin);
-    name[strlen(name)-1] = '\0'; /*remove the newline at the end*/
-
-<<<<<<< HEAD
-    printf("Hello, %s!\n", name);
+    char first[255], last[255];
+    askname(first, last);
+    printf("Hello, %s %s!\n", first, last);
     return 0;
 }
+
+/* askname.c*/
+#include <stdio.h>
+#include <string.h>
+
+void askname(char *first, char *last)
+{
+    printf("Enter your first name:");
+    fgets(first,255,stdin);
+    first[strlen(first)-1]='\0'; /*remove the newline at the end*/
+
+    printf("Now enter your last name:");
+    get(last); /*buffer overflow? what's that? */
+}
+
+/* askname.h */
+void askname(char *first, char *first);
